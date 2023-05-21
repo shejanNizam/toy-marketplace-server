57c189359db6e6748fe999b71e01d0b8a9af7e0d
shejanNizam <nizam.shejan11@gmail.com>
2023-05-21 12:52:59 +0600

    final submit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 7000;
